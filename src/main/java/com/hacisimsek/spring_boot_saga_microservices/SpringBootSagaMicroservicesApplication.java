897fac28559b5a341c7c203a48ea6c6e193b4b28
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2025-05-11 12:00:36 +0300

    init commit

---
+++ src/main/java/com/hacisimsek/spring_boot_saga_microservices/SpringBootSagaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.hacisimsek.spring_boot_saga_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSagaMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSagaMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hacisimsek/spring_boot_saga_microservices/SpringBootSagaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacisimsek.spring_boot_saga_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSagaMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
